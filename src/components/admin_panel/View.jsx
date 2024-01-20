c8e967487a758febd0092650c16822d2b24daa3a
Sam Windsor <121157983+windsywinds@users.noreply.github.com>
2024-01-20 16:03:02 +1300

    remove docs

--- src/components/admin_panel/View.jsx
+++ src/components/admin_panel/View.jsx
@@ -8,6 +8,7 @@ export const View = () => {
   return (
     <div className="bg-white flex flex-col w-full">
       <div className="flex flex-col my-4 mx-2 gap-6">
+        <p>Updated with YML working</p>
         <DisplayStats />
         <ManageProject />
       </div>
--- src/firebase.config.jsx
+++ src/firebase.config.jsx
@@ -8,13 +8,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getMessaging } from "firebase/messaging";
 
 
-const API_KEY = import.meta.env.VITE_API_KEY
-const AUTH_KEY = import.meta.env.VITE_AUTH_DOMAIN
-const PROJECT_ID = import.meta.env.VITE_PROJECT_ID
-const STORAGE_BUCKET = import.meta.env.VITE_STORAGE_BUCKET
-const MSG_SND_ID = import.meta.env.VITE_MESSAGING_ID
-const APP_ID = import.meta.env.VITE_APP_ID
-const MEASUREMENTS_ID = import.meta.env.VITE_MEASUREMENT_ID
+const API_KEY = import.meta.env.VITE_API_KEY || import.meta.env.VITE_API_KEY_LOCAL
+const AUTH_KEY = import.meta.env.VITE_AUTH_DOMAIN || import.meta.env.VITE_AUTH_DOMAIN_LOCAL
+const PROJECT_ID = import.meta.env.VITE_PROJECT_ID || import.meta.env.VITE_PROJECT_ID_LOCAL
+const STORAGE_BUCKET = import.meta.env.VITE_STORAGE_BUCKET || import.meta.env.VITE_STORAGE_BUCKET_LOCAL
+const MSG_SND_ID = import.meta.env.VITE_MESSAGING_ID || import.meta.env.VITE_MESSAGING_ID_LOCAL
+const APP_ID = import.meta.env.VITE_APP_ID || import.meta.env.VITE_APP_ID_LOCAL
+const MEASUREMENTS_ID = import.meta.env.VITE_MEASUREMENT_ID || import.meta.env.VITE_MEASUREMENT_ID_LOCAL
 
 const firebaseConfig = {
     apiKey: API_KEY,
