82dfcfc64b8d168e96dae5a8475fbf56c08a126b
Sam Windsor <121157983+windsywinds@users.noreply.github.com>
2024-01-20 16:32:55 +1300

    add hover tips

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 import background from "/images/herobg.png";
+import { useState, useEffect, useRef } from "react";
 
 const Hero = ({ scrollToContact, refHero }) => {
+  const [downloadTip, setDownloadTip] = useState(false)
+  const [contactTip, setContactTip] = useState(false)
+  const downloadRef = useRef(null);
+  const contactRef = useRef(null);
+
+  const handleTips = (event) => {
+    if (downloadRef.current && !downloadRef.current.contains(event.target)) {
+      setDownloadTip(false);
+    } else if (contactRef.current && !contactRef.current.contains(event.target)) {
+      setContactTip(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mouseenter', handleTips);
+
+    return () => {
+      document.removeEventListener('mouseenter', handleTips);
+    };
+  }, []);
+
   return (
     <div className="" ref={refHero}>
       <div
@@ -21,7 +43,10 @@ const Hero = ({ scrollToContact, refHero }) => {
             </p>
 
             <div className="flex flex-row text-right items-center justify-end gap-3 pr-1">
-              <a href="/downloads/Sam-Windsor-CV.pdf" download>
+              <a href="/downloads/Sam-Windsor-CV.pdf" download 
+                onMouseEnter={() => setDownloadTip(true)}
+                onMouseLeave={() => setDownloadTip(false)}
+                ref={downloadRef}>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -36,9 +61,13 @@ const Hero = ({ scrollToContact, refHero }) => {
                     d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m.75 12l3 3m0 0l3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
                   />
                 </svg>
+                
               </a>
 
-              <button onClick={() => scrollToContact()}>
+              <button onClick={() => scrollToContact()}
+              onMouseEnter={() => setContactTip(true)}
+              onMouseLeave={() => setContactTip(false)}
+              ref={contactRef}>
                 <svg
                   className="md:w-11 md:h-11 w-5 h-5 opacity-85"
                   fill="currentColor"
@@ -53,7 +82,17 @@ const Hero = ({ scrollToContact, refHero }) => {
                     d="M2.8 4C1.86451 4 1.0468 4.46923 0.544325 5.16792C0.20074 5.64567 0 6.23499 0 6.86667V17.1333C0 18.682 1.21964 20 2.8 20H10.2C10.7523 20 11.2 19.5523 11.2 19C11.2 18.4477 10.7523 18 10.2 18H2.8C2.39214 18 2 17.6466 2 17.1333V7.94766L7.77948 14.3096C8.96986 15.6199 11.0301 15.6199 12.2205 14.3096L18 7.94766V12.1333C18 12.6856 18.4477 13.1333 19 13.1333C19.5523 13.1333 20 12.6856 20 12.1333V6.86667C20 6.235 19.7993 5.64567 19.4557 5.16792C18.9532 4.46923 18.1355 4 17.2 4H2.8ZM9.25983 12.9647L2.9327 6H17.0673L10.7402 12.9647C10.3434 13.4015 9.65662 13.4015 9.25983 12.9647ZM18.2929 21.2929C17.9024 21.6834 17.9024 22.3166 18.2929 22.7071C18.6834 23.0976 19.3166 23.0976 19.7071 22.7071L22.7071 19.7071C23.0976 19.3166 23.0976 18.6834 22.7071 18.2929L19.7071 15.2929C19.3166 14.9024 18.6834 14.9024 18.2929 15.2929C17.9024 15.6834 17.9024 16.3166 18.2929 16.7071L19.5858 18H15C14.4477 18 14 18.4477 14 19C14 19.5523 14.4477 20 15 20H19.5858L18.2929 21.2929Z"
                   ></path>{" "}
                 </svg>
+                
               </button>
+            </div>
+            <div className="flex flex-row text-right items-center justify-end gap-3 pr-1">
+            {downloadTip &&
+                  <span className="bg-slate-800 rounded-xl drop-shadow-lg absolute text-xs mt-8 px-6 py-2 z-1">Download CV</span>
+              }
+            {contactTip &&
+                  <span className="bg-slate-800 rounded-xl drop-shadow-lg absolute text-xs mt-8 px-6 py-2 z-1">Jump to Contact Form</span>
+              }
+
             </div>
           </div>
         </div>
