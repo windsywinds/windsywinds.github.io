f1c9e0d983cf244cfeec909e86c27134ffba8e66
Sam Windsor <121157983+windsywinds@users.noreply.github.com>
2024-04-28 21:18:34 +1200

    fix: update intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,16 +3,14 @@ import React from "react";
 function Intro() {
   return (
     <div className="intro">
-      <div className="flex items-center justify-center flex-col text-center pt-20 pb-16 pl-10 pr-10">
+      <div className="flex items-start justify-center flex-col text-left pt-20 pb-16 pl-10 pr-10">
         <p className="text-sm mb-2 font-bold">
           Hi! I'm Sam, a Developer, Photographer, and Snowboard coach. After
           hanging up my snowboard boots for a career change, I completed a
-          Diploma in Web Development and Design to kickstart my learning and am
-          currently participating in the Mission Ready Cloud and DevOps
-          Developer program. I primarily work with ReactJS and other frontend
-          Javascript frameworks, however I do have some some experience with
-          languages such as Python, C#, and MySQL, which I've had the chance to
-          experience during my studies.
+          Diploma in Web Development and Design to kickstart my learning and then went on to complete the Mission Ready Cloud and DevOps
+          Developer program. <br/>
+          I primarily work in React.js and other frontend
+          Javascript frameworks, but have experience across a number of technology stacks, previously completing projects using Python, C#, and MySQL.
         </p>
         <p className="text-sm mb-2 font-bold pt-2">
           In my spare time I enjoy getting out for hikes to take photos,
@@ -22,8 +20,8 @@ function Intro() {
           app one day.
         </p>
         <p className="text-sm mb-6 font-bold pt-5">
-          I've included some projects I've worked on below and included a
-          contact form to get in touch. I'd love to work with you and your team!
+          I've included some projects I've worked on below and also a
+          contact form to get in touch. I'd love to hear from you!
         </p>
       </div>
     </div>
