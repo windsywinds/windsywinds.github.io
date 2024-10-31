a5afc35e31f19996fe89d68b3b9577d65f30bcd9
windsywinds <windsor.sam@gmail.com>
2024-10-31 21:17:59 +1300

    fix: enable download

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -37,9 +37,30 @@ const Hero = ({ scrollToContact, refHero }) => {
 
             <div className="flex flex-row text-right items-center justify-end gap-3 pr-1">
 
-            
-
-              
+            <HoverTip 
+            tip={'Download CV'}
+            position={"bottom-middle"}
+            >            
+              <a href="/downloads/Sam-Windsor-CV.pdf" download 
+              className="flex items-center justify-center"
+                >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="2"
+                  stroke="currentColor"
+                  className="md:w-10 md:h-10 w-5 h-5 opacity-85"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m.75 12l3 3m0 0l3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
+                  />
+                </svg>
+                
+              </a>
+              </HoverTip>
 
               <HoverTip 
               tip={'Jump to Contact Form'}
