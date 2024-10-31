60ae6e6e77cd8e9984575f552b97988deffb6ba9
windsywinds <windsor.sam@gmail.com>
2024-10-31 21:16:58 +1300

    fix: unused imports

--- src/components/statistics/DisplayStats.jsx
+++ src/components/statistics/DisplayStats.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { db, auth, googleProvider } from "../../firebase.config";
+import { db } from "../../firebase.config";
 import {
   doc,
   setDoc,
